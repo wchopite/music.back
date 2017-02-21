00d1c369da06186d3695fd6a7633462ef2326f93
Wladimir Chopite <wchopite@gmail.com>
2017-02-21 09:18:16 -0400

    Relaciones de modelos
    * Establecidas las relaciones entre los modelos
    * Creados los controladores con la opcion --resource

--- app/Artist.php
+++ app/Artist.php
@@ -4,7 +4,21 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Artist extends Model
-{
-    //
+class Artist extends Model {
+
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	 protected $fillable = ['name'];
+
+	 /**
+	  * [albums description]
+	  * @return [type] [description]
+	  */
+	 public function albums() {
+
+		 return $this->hasMany('App\Album');
+	 }
 }
--- app/Gender.php
+++ app/Gender.php
@@ -4,7 +4,21 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Gender extends Model
-{
-    //
+class Gender extends Model {
+
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	 protected $fillable = ['name'];
+
+	 /**
+	  * [albums description]
+	  * @return [type] [description]
+	  */
+	 public function albums() {
+
+		return $this->hasMany('App\Album');
+	 }
 }
